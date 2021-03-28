df5db084a69842fd961a1ee3a9e9fb896523c2b4
l-1cm-l <l-----1.5cm----l@i.softbank.jp>
2021-03-28 11:55:42 +0900

    Initial Commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by administrator on 2021/03/28.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
